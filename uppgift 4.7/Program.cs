a8423a615e0e243ce5177b54053917d87a9c9eae
en301 <emilsjostrand1000@gmail.com>
2023-10-19 20:26:43 +0200

    Add project files.

---
+++ uppgift 4.7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace uppgift_4_7
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett meddelande");
+            string meddelande = Console.ReadLine();
+
+            Console.WriteLine("Skriv in hur många steg meddelandet ska ligga");
+            int steg = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < meddelande.Length; i++)
+            {
+                for (int j = 0; j < steg; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(meddelande[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
